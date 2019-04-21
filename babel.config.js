0e25acb08799f69276677c2ee25749cee6c39264
Joshua Robinson <jbrobin2@gmail.com>
2019-04-21 13:39:28 -0400

    compile down spread syntax

--- babel.config.js
+++ babel.config.js
@@ -3,13 +3,13 @@ module.exports = {
     [
       "@babel/preset-env",
       {
-        "targets": {
-          "node": "8.10"
-        }
+        "targets": "> 0.25%, not dead"
       }
     ]
   ],
   "plugins": [
-    "@babel/plugin-proposal-class-properties"
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-transform-destructuring",
+    "@babel/plugin-proposal-object-rest-spread"
   ]
 }
